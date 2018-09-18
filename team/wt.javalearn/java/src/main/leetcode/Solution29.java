e513dcc0ee35f32acd848de833d6ee3e7b244e7e
Wh1t3zZ <30459692+youboyTizzyT@users.noreply.github.com>
2018-09-18 17:03:43 +0800

    提交代码

--- team/wt.javalearn/java/src/main/leetcode/Solution29.java
+++ team/wt.javalearn/java/src/main/leetcode/Solution29.java
@@ -8,14 +8,15 @@ public class Solution29 {
     public static void main(String[] args) {
         Solution29 solution29=new Solution29();
         System.out.println(solution29.divide(-2147483648,-1));
+//        System.out.println((-2147483648)/(-1));
     }
 
     public int divide(int dividend, int divisor) {
         int ret=0;
 
-        if (Math.abs(divisor)==1){
-            ret= dividend*divisor;
-        }
+//        if (Math.abs(divisor)==1){
+//            ret= dividend*divisor;
+//        }
         //符号位
         int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
 
@@ -31,15 +32,15 @@ public class Solution29 {
                 tmp <<= 1;
                 mul <<= 1;
             }
-
             //减去最接近dvd的dvs的指数倍数的值（值为tmp）
             dvd -= tmp;
-
-            ret = ret+mul<ret?Integer.MAX_VALUE: (int) (ret + mul);
-
+            System.out.println(ret);
+            ret= ret+mul<ret?Integer.MAX_VALUE: (int) (ret + mul);
+//            ret +=mul;
         }
-        if (ret==Integer.MIN_VALUE&&sign==-1){
-            return Integer.MAX_VALUE;
+        System.out.println(ret);
+        if (ret<0&&sign==1){
+            ret=Integer.MAX_VALUE;
         }
         return ret*sign;
     }
