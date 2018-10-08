7b5296d09f3ec915c1b71b55832014ada35c72fd
Wh1t3zZ <30459692+youboyTizzyT@users.noreply.github.com>
2018-10-08 15:31:45 +0800

    提交算法

--- team/wt.javalearn/java/src/main/leetcode/Solution21.java
+++ team/wt.javalearn/java/src/main/leetcode/Solution21.java
@@ -7,50 +7,62 @@ package leetcode;
  */
 public class Solution21 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1==null&&l2==null){
+            return null ;
+        }
+        if (l1==null||l2==null){
+            return l1==null?l2:l1;
+        }
+        //123
         ListNode small=l1.val>l2.val?l2:l1;
+        //145
         ListNode max=l1.val>l2.val?l1:l2;
         ListNode ret=small;
-        while (small!=null&&max!=null){
-            if (small.next!=null&&max!=null){
-                if (small.next.val>max.val){
-                    max=swap(small,max);
-                }else {
-                    small=small.next;
-                }
+        while (max != null) {
+            if (small == null) {
+                small = max;
+                break;
             }
+            if(small.next==null){
+                small.next=max;
+                break;
+            }
+            if (small.val <= max.val && small.next.val >= max.val) {
+                ListNode tmp1 = small.next;
+                small.next = max;
+                ListNode tmp = max;
+                max = max.next;
+                tmp.next = tmp1;
+                small = tmp;
+                continue;
+            }
+            small = small.next;
         }
         return ret;
     }
-    public ListNode swap(ListNode small,ListNode max){
-        ListNode tmp=max;
-        ListNode tmp2=max.next;
-        ListNode tmp1=small.next;
-        small.next=tmp;
-        tmp.next=tmp1;
-        return tmp2;
-    }
+
 
     public static void main(String[] args) {
-        ListNode l1=new ListNode(1);
+//        ListNode l1=new ListNode(1);
         ListNode l2=new ListNode(2);
-        ListNode l3=new ListNode(4);
+//        ListNode l3=new ListNode(4);
 //        ListNode l4=new ListNode(6);
 //        ListNode l5=new ListNode(7);
         ListNode l6=new ListNode(1);
-        ListNode l7=new ListNode(3);
-        ListNode l8=new ListNode(4);
+//        ListNode l7=new ListNode(3);
+//        ListNode l8=new ListNode(4);
 //        ListNode l9=new ListNode(5);
 //        ListNode l0=new ListNode(6);
-        l1.next=l2;
-        l2.next=l3;
+//        l1.next=l2;
+//        l2.next=l3;
 //        l3.next=l4;
 //        l4.next=l5;
-        l6.next=l7;
-        l7.next=l8;
+//        l6.next=l7;
+//        l7.next=l8;
 //        l8.next=l9;
 //        l9.next=l0;
         Solution21 solution21=new Solution21();
-        ListNode ret=solution21.mergeTwoLists(l1,l6);
+        ListNode ret=solution21.mergeTwoLists(l2,l6);
         while (ret!=null){
             System.out.println(ret.val);
             ret=ret.next;
